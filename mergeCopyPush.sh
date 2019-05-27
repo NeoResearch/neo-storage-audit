b7135ad6ec9231e580f76c04e25f56a63f0027b5
neoresearch-compilers-neotests <neoresearch-compilers-neotests@neoresearch.io>
2019-05-27 22:20:37 +0000

    Persisting and Re-Persisting storage with neo 2.10.2 stable

--- mergeCopyPush.sh
+++ mergeCopyPush.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 (cd ~/neo-tests/simple-rpc-node && ./merge_Storage.sh)
-cp -r ~/neo-tests/simple-rpc-node/Storage/* ./
+cp -r ~/neo-tests/simple-rpc-node/Storage_mainnet/* ./
 
 git add .
 git commit -m "Persisting storage"
